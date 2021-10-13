e2c9d698e3e8385c960db5a39acaf754cde75dc7
gaotingquan <gaotingquan@baidu.com>
2021-10-13 14:36:40 +0000

    add baseline config

--- tools/train.sh
+++ tools/train.sh
@@ -4,4 +4,6 @@
 # python3.7 tools/train.py -c ./ppcls/configs/ImageNet/ResNet/ResNet50.yaml
 
 # for multi-cards train
-python3.7 -m paddle.distributed.launch --gpus="0,1,2,3" tools/train.py -c ./ppcls/configs/ImageNet/ResNet/ResNet50.yaml
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+python -m paddle.distributed.launch --gpus="0,1,2,3" tools/train.py -c ./ppcls/configs/Rec/ResNet50_vd_SOP.yaml
+
